02cb6260dcad353615443ee89f7c0f7e5674f209
Polina <polina.liutsko@gmail.com>
2020-05-21 21:09:34 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Inkscape.logo.svg/390px-Inkscape.logo.svg.png" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://www.pictorem.com/collection/900_fabartdesigns_tree,%20sunset,%20amazing,%20beautiful,%20breathtaking,%20colorful,%20evening,%20lonely,%20nature,%20peace,%20peaceful,%20red,%20romantic,%20sky,%20island,%20sundown,%20sea,%20ocean,.jpg" />
+      </div>
+      <div>ava+description</div>
+      <div>
+        My posts
+        <div>New posts</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
